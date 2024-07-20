1cdd7739b02ed200bbc0876a16c6c5e019d60bd2
Kayo Souza <kayo.felipe.souza2014@gmail.com>
2024-07-20 18:30:26 -0300

    Chande products list table style

--- produtos/listar.php
+++ produtos/listar.php
@@ -32,8 +32,7 @@ mysqli_close($connection);
 		<title>Cadastrar produto - Estoque</title>
 		<script src="https://cdn.tailwindcss.com"></script>
 		<link rel="stylesheet" href="../styles/global.css">
-		<script src="https://unpkg.com/tabulator-tables@4.1.4/dist/js/tabulator.min.js"></script>
-		<link rel="stylesheet" href="https://unpkg.com/tabulator-tables@4.1.4/dist/css/tabulator.min.css">
+		<link rel="stylesheet" href="../styles/tabulator.css">
 	</head>
 	<body class="bg-gray-700 text-gray-50 min-h-dvh">
 		<main>
@@ -44,8 +43,9 @@ mysqli_close($connection);
 				</hgroup>
 			</header>
 
-			<div id="lista" class="mt-8 w-11/12 mx-auto"></div>
+			<div id="lista" class="w-11/12 mt-8 mb-4 mx-auto"></div>
 
+			<script src="https://unpkg.com/tabulator-tables@4.1.4/dist/js/tabulator.min.js"></script>
 			<script>
 				(() => {
 				const produtos = <?php echo json_encode($produtosArray) ?>
@@ -61,15 +61,15 @@ mysqli_close($connection);
 						{ column: "id", dir: "asc" }
 					],
 					columns: [
-						{ field: "id", width: 20, headerFilter: "input", hozAlign: "center" },
+						{ field: "id", title: "ID", width: 20, headerFilter: "input", hozAlign: "center" },
 						{ field: "nome", title: "Nome", headerFilter: "input" },
 						{ field: "numero", title: "Número", width: 90, headerFilter: "input", hozAlign: "center" },
 						{ field: "categoria", title: "Categoria", headerFilter: "input" },
 						{ field: "fornecedor", title: "Fornecedor", headerFilter: "input" },
 						{ field: "quantidade", title: "Quantidade", width: 120, headerFilter: "input", hozAlign: "center" },
 						{ title: "Ações", width: 108, headerSort: false, formatter: cell => (
-							`<a href="editar.php?id=${cell.getRow().getData().id}" class="bg-yellow-600 text-white inline-flex items-center text-center px-1 rounded select-none" role="button">Editar</button>` +
-							`<a href="excluir.php?id=${cell.getRow().getData().id}" class="bg-red-600 text-white inline-flex items-center text-center px-1 ml-1 rounded select-none" role="button">Excluir</button>`
+							`<a href="editar.php?id=${cell.getRow().getData().id}" class="bg-yellow-600 hover:opacity-90 hover:shadow focus:bg-yellow-700 text-white inline-flex items-center text-center px-1 rounded select-none" role="button">Editar</button>` +
+							`<a href="excluir.php?id=${cell.getRow().getData().id}" class="bg-red-600 hover:opacity-90 hover:shadow focus:bg-red-700 text-white inline-flex items-center text-center px-1 ml-1 rounded select-none" role="button">Excluir</button>`
 						), columnVertAlign: "center", hozAlign: "center" }
 					],
 					locale: "pt-BR",
