646dd6fecb559c9a82b808449d9a581455c831e1
Kayo Souza <kayo.felipe.souza2014@gmail.com>
2024-07-25 17:50:34 -0300

    Move database information into .env file

--- conexao.php
+++ conexao.php
@@ -1,15 +1,15 @@
 <?php
 
-$host = "localhost";
-$port = 3306;
-$user = "root";
-$password = "root";
-$dbName = "estoque";
+$env = parse_ini_file(".env");
 
-$connection = mysqli_connect($host, $user, $password, $dbName, $port);
+$DB_HOST = $env["HOST"];
+$DB_NAME = $env["DB_NAME"];
+$DB_USER = $env["DB_USER"];
+$DB_PORT = $env["DB_PORT"];
+$DB_PASSWORD = $env["DB_PASSWORD"];
+
+$connection = mysqli_connect($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME, $DB_PORT);
 
 if($connection->connect_error){
 	die("Falha ao conectar ao banco de dados: " . $conn->connect_error);
 }
-
-?>
